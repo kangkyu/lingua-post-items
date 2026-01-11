129de98c2d7379aba3c654dfb43c43d6b23361a8
Kang-Kyu Lee <kangkyu1111@gmail.com>
2026-01-11 03:07:53 -0800

    Revert "Console log temporary"
    This reverts commit 94afd119859b44bbb68a0c1fca7d1ecdf0a256e2.

--- backend/translate.js
+++ backend/translate.js
@@ -21,7 +21,6 @@ export default async function handler(req, res) {
   }
 
   try {
-    console.log('Translation request - projectId:', projectId, 'location:', location);
     const request = {
       parent: `projects/${projectId}/locations/${location}`,
       contents: [text],
